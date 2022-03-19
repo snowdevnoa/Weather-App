9cb123c36c05cc29f003116df27aa3ca91d6e11c
xmasnoaa <xmas.noaa@gmail.com>
2022-03-19 01:34:40 -0500

    DOM manipulation and event listeners, CSS touch up

--- script.js
+++ script.js
@@ -16,15 +16,31 @@ let weather = {
     const { temp, humidity } = data.main;
     const { speed } = data.wind; //data is taken out of that object and made into a variable
     console.log(name, icon, description, temp, humidity, speed);
-    document.getElementById('city').innerText = "Weather in " + name;
-    document.getElementById('temp').innerText = Math.round(temp) + "°C";
-    document.getElementById('icon').src =
+    document.getElementById("city").innerText = "Weather in " + name;
+    document.getElementById("temp").innerText = Math.round(temp) + "°C";
+    document.getElementById("icon").src =
       "http://openweathermap.org/img/wn/" + icon + ".png";
-    document.getElementById('description').innerText = description;
-    document.getElementById('humidity').innerText = "Humidity: " + humidity + "%";
-    document.getElementById('wind').innerText = "Wind speed: " + speed + " km/h";
-
-
-
+    document.getElementById("description").innerText = description;
+    document.getElementById("humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.getElementById("wind").innerText =
+      "Wind speed: " + speed + " km/h";
+    document.querySelector(".weather").classList.remove("loading");
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/3840x2160/?" + name + "')";
+  },
+  search: function () {
+    this.fetchWeather(document.getElementById("search-bar").value);
   },
 };
+
+document.querySelector("#search button").addEventListener("click", function () {
+  weather.search();
+});
+document
+  .getElementById("search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
